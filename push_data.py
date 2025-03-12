949fe36d68c3ce380d7922ea506051684b126a7f
Sookchand <34344439+Sookchand@users.noreply.github.com>
2025-03-12 14:52:00 -0400

    ETL pipeline completed

--- push_data.py
+++ push_data.py
@@ -1,14 +1,58 @@
-# Description: This script is used to push data to MongoDB Atlas.
-from pymongo.mongo_client import MongoClient
+import os
+import sys
+import json 
 
-uri = "mongodb+srv://sookcharri:<tXtgPdDiO1frcUFa>@cluster0.5bojx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+from dotenv import load_dotenv
+load_dotenv()
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+import certifi
+ca=certifi.where()
+
+import pandas as pd
+import pymongo  
+import numpy as np
+from networksecurity.exception.exception import NetworkSecurityException    
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def csv_to_json_converter(self, file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = json.loads(data.T.to_json()).values()  # Use json.loads instead of json.load)
+            return list(records)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)   
+        
+    def insert_data_mongodb(self, records, database, collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+            
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]            
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+if __name__ == "__main__":
+    FILE_PATH = "Network_Data\phisingData.csv"  
+    DATABASE = "network_security"
+    Collection = "NetworkData"
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_converter(file_path=FILE_PATH)
+    print(records)
+    no_of_records = networkobj.insert_data_mongodb(records, DATABASE, Collection)
+    print(f"Number of records inserted in MongoDB: {no_of_records}")
